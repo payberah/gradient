ee1ba29d32aa6a8382229eb836bc0fe36011dd0f
Amir H. Payberah <amir@sics.se>
2015-05-27 15:54:00 +0200

    delete

--- kompics/peer/gradinet/PeerSamplingPort.java
+++
@@ -1,8 +0,0 @@
-package kompics.peer.gradinet;
-
-import se.sics.kompics.PortType;
-
-public final class PeerSamplingPort extends PortType {{
-	negative(Join.class);
-	positive(JoinCompleted.class);
-}}
